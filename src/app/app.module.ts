ad80e46d670560524331544523d1bcf99ab90dde
Joseph Genchik <jgenchik@gmail.com>
2019-06-10 22:34:49 -0400

    added empty route object

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { SharedModule } from './shared/shared.module';
+
+
+const appRoutes: Routes = [
+  // { path: 'welcome', component: WelcomeComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  // {
+  //   path: 'r-light',
+  //   component: RLightComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+
+  // { path: '**', component: PageNotFoundComponent }
+];
+
 
 @NgModule({
   declarations: [
@@ -15,7 +31,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     BrowserAnimationsModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    SharedModule
+    FlexLayoutModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: false, useHash: false }),
   ],
   providers: [],
   bootstrap: [AppComponent]
